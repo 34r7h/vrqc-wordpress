5b5a84d7a5de41478157ae22579cbbd35968e7bf
irthos <irth03@gmail.com>
2015-01-20 23:34:58 -0800

    battle of the theme continues

--- wp-content/themes/vrqc/home.php
+++ wp-content/themes/vrqc/home.php
@@ -1,14 +1,13 @@
 <?php get_header(); ?>
-<?php
-
-$args = array( 'post_type' => 'attachment', 'posts_per_page' => -1, 'post_status' =>'any', 'post_parent' => $post->ID );
-$attachments = get_posts( $args );
-if ( $attachments ) {
-foreach ( $attachments as $attachment ) {
-echo apply_filters( 'the_title' , $attachment->post_title );
-the_attachment_link( $attachment->ID , false );
-}
-}
-?>
-Blogroll
+<div class="container">
+    <?php
+        $attachments = get_posts( );
+        foreach ( $attachments as $attachment ) {
+            $id = $attachment->ID;
+            echo '<div class="col-xs-4"> ' . apply_filters( 'the_title' , $attachment->post_title );
+            echo get_the_post_thumbnail($id, 'full', array('class'=>'col-xs-12')) . '</div>';
+        }
+    ?>
+    Blogroll
+</div>
 <?php get_footer(); ?>
