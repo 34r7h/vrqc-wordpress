8430a10cc14ae4241bee49ecefdaa0c2d2138609
free-info <irth03@gmail.com>
2015-04-03 04:30:08 -0700

    polishing theme

--- wp-content/themes/vrqc/header.php
+++ wp-content/themes/vrqc/header.php
@@ -17,27 +17,36 @@
 <header class="clearfix">
     <nav class="navbar navbar-default navbar-fixed-top top-nav">
         <div class="col-xs-12">
-            <a class="navbar-brand" href="<?php echo get_site_url(); ?>">
+            <a class="pull-left navbar-text" href="<?php echo get_site_url(); ?>">
                 <img class="pull-left" style="width: 40px; height: auto;" src="<?php echo( get_header_image() ); ?>" alt="<?php echo( get_bloginfo( 'title' ) ); ?>" />
-                <span class="">Vacation Rentals Quebec City</span>
+                <span style="font-size: 1.3em" class="nav-brand">Vacation Rentals Quebec City</span>
             </a>
-            <a class="pull-right" href="tel:15817776339">
-                <button style="font-size: 120%" class="fa fa-phone phone btn btn-default navbar-btn"> <span class="hidden-xs">+01-581-777-6339</span></button>
+            <a class="pull-left navbar-text" target="new"  href="<?php echo get_site_url(); ?>">
             </a>
-            <span class="pull-right language navbar-btn"></span>
+            <div class="pull-right" >
+                <a href="tel:15817776339" class="fa fa-phone phone btn btn-default navbar-btn"> <span style="font-size: 120%" class="hidden-xs">+01-581-777-6339</span></a>
+            </div>
         </div>
+
     </nav>
     <hr/>
     <nav class="navbar navbar-default">
         <br/>
+
             <!-- Brand and toggle get grouped for better mobile display -->
-            <ul class="navbar-header col-xs-12 col-sm-6">
+            <ul class="navbar-header col-xs-12 col-sm-7">
+                <h1 class="col-xs-12 text-center tagline">We strive to provide a most uniquely Quebecois experience!</h1>
+                <div class="col-xs-12 btn-group btn-group-justified" ng-init="nav.pages = ['Home','About','Properties','Contact','Blog']">
+                    <a ng-repeat="(key,page) in nav.pages" ng-href="<?php echo get_site_url(); ?>/{{page}}" type="button" class="btn btn-default">{{page}}</a>
+                </div>
+<!--
                     <?php wp_nav_menu( array( 'items_wrap' => '<a id="%1$s" class="%2$s">%3$s</a>', 'depth' => 0) ); ?>
+-->
             </ul>
 
             <!-- Collect the nav links, forms, and other content for toggling -->
 
-            <div class="col-xs-12 col-sm-6 pull-right">
+            <div class="col-xs-12 col-sm-5 pull-right">
                 <div class="featured-offer">
                     <?php
                 $cat_id = 5; //the certain category ID
@@ -55,12 +64,12 @@
         <div class="col-xs-12">
             <a class="pull-left navbar-text" href="mailto:quebeccityreservations@gmail.com">
                 <button class="btn btn-default">
-                    <i class="fa fa-envelope"> <span class="hidden-xs">Email</span></i>
+                    <i style="font-size: 120%" class="fa fa-envelope"> <span class="hidden-xs">Email</span></i>
                 </button>
             </a>
             <a class="pull-left navbar-text" target="new" href="https://www.facebook.com/pages/Vacation-Rentals-Quebec-City/188400344615871">
                 <button class="btn btn-default">
-                    <i class="fa fa-facebook-official"> <span class="hidden-xs">Facebook</span></i>
+                    <i style="font-size: 120%" class="fa fa-facebook-official"> <span class="hidden-xs">Facebook</span></i>
                 </button>
             </a>
             <div class="weather pull-right navbar-text" style="font-size: 120%" >
